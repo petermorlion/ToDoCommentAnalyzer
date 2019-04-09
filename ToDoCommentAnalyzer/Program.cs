9ffd868ff35346b2f63594942212543e432457dc
Peter Morlion <peter.morlion@gmail.com>
2019-04-09 13:39:39 +0200

    Create a new empty project

---
+++ ToDoCommentAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoCommentAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
